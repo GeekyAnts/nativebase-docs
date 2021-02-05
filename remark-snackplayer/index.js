ac508abdb6339b10839b8df12599c7f73e17e6aa
Ankur Kedia <theankurkedia@gmail.com>
2021-02-05 12:48:52 +0530

    Changed NB version

--- remark-snackplayer/index.js
+++ remark-snackplayer/index.js
@@ -28,7 +28,7 @@ const processNode = (node, parent) => {
       //         metaArr[0].length
       //       )
       //     : 'next';
-      const NBversion = '3.0.0-next.19';
+      const NBversion = '3.0.0-next.21';
       // Gather necessary Params
       let name = params.name ? decodeURIComponent(params.name) : 'Example';
       // name = name
