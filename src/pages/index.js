a9c65ddad36614b84a9596665dd2406accc245d5
Ankur Kedia <theankurkedia@gmail.com>
2021-02-10 11:11:57 +0530

    Fixes for icon size on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,55 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import styles from "./styles.module.css";
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
 
 const features = [
   {
-    title: "Accessible",
-    imageUrl: "/img/NB-feature-icons/SVG/Accessible.svg",
-    description: "Maximise app usage with our accessible design.",
+    title: 'Accessible',
+    imageUrl: '/img/NB-feature-icons/SVG/Accessible.svg',
+    description: 'Maximise app usage with our accessible design.',
   },
   {
-    title: "Responsive",
-    imageUrl: "/img/NB-feature-icons/SVG/Responsive.svg",
-    description: "Create responsive apps with ease from the get-go.",
+    title: 'Responsive',
+    imageUrl: '/img/NB-feature-icons/SVG/Responsive.svg',
+    description: 'Create responsive apps with ease from the get-go.',
   },
   {
-    title: "Themeable",
-    imageUrl: "/img/NB-feature-icons/SVG/Themeable.svg",
-    description: "Customise your app theme and component styles.",
+    title: 'Themeable',
+    imageUrl: '/img/NB-feature-icons/SVG/Themeable.svg',
+    description: 'Customise your app theme and component styles.',
   },
   {
-    title: "Consistent",
-    imageUrl: "/img/NB-feature-icons/SVG/Consistent.svg",
-    description: "Make accurate predictions for web, Android & iOS.",
+    title: 'Consistent',
+    imageUrl: '/img/NB-feature-icons/SVG/Consistent.svg',
+    description: 'Make accurate predictions for web, Android & iOS.',
   },
 ];
 
 const Features = () => (
-  <div className="container">
-    <div className="row">
+  <div className='container'>
+    <div className='row'>
       {features.map((feature, key) => {
         return (
-          <div className={clsx("col col--3", styles.textAlignCenter)}>
+          <div className={clsx('col col--3', styles.textAlignCenter)}>
             <img
               src={feature.imageUrl}
-              height="58px"
-              width="80px"
+              height='58px'
+              width='80px'
               className={clsx(styles.image)}
             ></img>
             <h3
               style={{
-                fontSize: "20px",
+                fontSize: '20px',
                 letterSpacing: 0,
               }}
             >
               {feature.title}
             </h3>
-            <p style={{ color: "#5e5e5e", fontWeight: "500" }}>
+            <p style={{ color: '#5e5e5e', fontWeight: '500' }}>
               {feature.description}
             </p>
           </div>
@@ -64,53 +64,53 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title} | A complete component library for React Native Ecosystem`}
-      description="A complete component library for React Native Ecosystem"
+      description='A complete component library for React Native Ecosystem'
     >
       <header className={clsx(styles.mainBanner)}>
-        <div className="container ">
+        <div className='container '>
           <div
             className={clsx(
-              "row",
+              'row',
               styles.justifyCenter,
               styles.alignCenter,
               styles.displayFlex
             )}
           >
             <img
-              src={"/img/native-base-icon.png"}
-              width="50px"
-              height="50px"
-              alt="Project Logo"
-              style={{ marginRight: "10px" }}
+              src={'/img/native-base-icon.png'}
+              width='47.5px'
+              height='55px'
+              alt='Nativebase'
+              style={{ marginRight: '10px' }}
             />
 
             <h1
               className={clsx(styles.marginVerticalAuto)}
-              style={{ fontSize: "48px" }}
+              style={{ fontSize: '48px' }}
             >
               {siteConfig.title}
               <span
                 style={{
-                  lineHeight: "14px",
-                  fontSize: "14px",
-                  marginLeft: "5px",
+                  lineHeight: '14px',
+                  fontSize: '14px',
+                  marginLeft: '5px',
                 }}
               >
                 (alpha)
               </span>
             </h1>
           </div>
-          <p style={{ marginTop: "20px", fontSize: "32px" }}>
+          <p style={{ marginTop: '20px', fontSize: '32px' }}>
             {siteConfig.tagline}
           </p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                "button button--secondary button--lg",
+                'button button--secondary button--lg',
                 styles.sizeSmall
               )}
-              to={useBaseUrl("docs/nativebase")}
-              style={{ color: "#3e3a6f" }}
+              to={useBaseUrl('docs/nativebase')}
+              style={{ color: '#3e3a6f' }}
             >
               GET STARTED
             </Link>
