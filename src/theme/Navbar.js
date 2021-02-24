1bf8d46d2203c311efd205ade83c70d3db0b8fbc
MD-REHMAN <Rehman.2468@gmail.com>
2021-02-24 16:42:20 +0530

    fix: updated header design and some icon fixes

--- src/theme/Navbar.js
+++ src/theme/Navbar.js
@@ -1,30 +1,30 @@
-import React, { useCallback, useState, useEffect } from "react";
-import clsx from "clsx";
-import SearchBar from "@theme/SearchBar";
-import Toggle from "@theme/Toggle";
-import useThemeContext from "@theme/hooks/useThemeContext";
-import { useThemeConfig } from "@docusaurus/theme-common";
-import useHideableNavbar from "@theme/hooks/useHideableNavbar";
-import useLockBodyScroll from "@theme/hooks/useLockBodyScroll";
-import useWindowSize, { windowSizes } from "@theme/hooks/useWindowSize";
-import NavbarItem from "@theme/NavbarItem";
-import Logo from "@theme/Logo";
-import IconMenu from "@theme/IconMenu";
-import Moon from "../img/Moon";
-import Sun from "../img/Sun";
-import GitHub from "../img/GitHub";
-import RightArrow from "../img/RightArrow";
-import styles from "./styles.module.css"; // retrocompatible with v1
+import React, { useCallback, useState, useEffect } from 'react';
+import clsx from 'clsx';
+import SearchBar from '@theme/SearchBar';
+import useThemeContext from '@theme/hooks/useThemeContext';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import useHideableNavbar from '@theme/hooks/useHideableNavbar';
+import useLockBodyScroll from '@theme/hooks/useLockBodyScroll';
+import useWindowSize, { windowSizes } from '@theme/hooks/useWindowSize';
+import NavbarItem from '@theme/NavbarItem';
+import Logo from '@theme/Logo';
+import IconMenu from '@theme/IconMenu';
+import Moon from '../img/Moon';
+import Sun from '../img/Sun';
+import GitHub from '../img/GitHub';
+import Discord from '../img/Discord';
+import RightArrow from '../img/RightArrow';
+import styles from './styles.module.css'; // retrocompatible with v1
 
-const DefaultNavItemPosition = "left"; // If split links by left/right
+const DefaultNavItemPosition = 'left'; // If split links by left/right
 // if position is unspecified, fallback to right (as v1)
 
 function splitNavItemsByPosition(items) {
   const leftItems = items.filter(
-    (item) => (item.position ?? DefaultNavItemPosition) === "left"
+    (item) => (item.position ?? DefaultNavItemPosition) === 'left'
   );
   const rightItems = items.filter(
-    (item) => (item.position ?? DefaultNavItemPosition) === "right"
+    (item) => (item.position ?? DefaultNavItemPosition) === 'right'
   );
   return {
     leftItems,
@@ -37,20 +37,6 @@ function Navbar() {
     navbar: { items, hideOnScroll, style },
     colorMode: { disableSwitch: disableColorModeSwitch },
   } = useThemeConfig();
-  const centerItems = [
-    {
-      to: "docs/nativebase",
-      // activeBasePath: "docs",
-      label: "Docs",
-      position: "center",
-    },
-    { to: "docs/faq", label: "Market", position: "center" },
-    {
-      to: "https://geekyants.com/",
-      label: "Hire Us",
-      position: "center",
-    },
-  ];
   const [sidebarShown, setSidebarShown] = useState(false);
   const [isSearchBarExpanded, setIsSearchBarExpanded] = useState(false);
   const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
@@ -73,18 +59,14 @@ function Navbar() {
     }
   }, [windowSize]);
   const { leftItems, rightItems } = splitNavItemsByPosition(items);
-  console.log("items - ", items);
-  console.log("leftItems - ", leftItems);
-  console.log("centerItems - ", centerItems);
-  console.log("rightItems - ", rightItems);
   return (
     <nav
       id="tailwind"
       ref={navbarRef}
-      className={clsx("navbar", "navbar--fixed-top", {
-        "navbar--dark": style === "dark",
-        "navbar--primary": style === "primary",
-        "navbar-sidebar--show": sidebarShown,
+      className={clsx('navbar', 'navbar--fixed-top', {
+        'navbar--dark': style === 'dark',
+        'navbar--primary': style === 'primary',
+        'navbar-sidebar--show': sidebarShown,
         [styles.navbarHideable]: hideOnScroll,
         [styles.navbarHidden]: !isNavbarVisible,
       })}
@@ -106,57 +88,86 @@ function Navbar() {
           <Logo
             className="navbar__brand"
             imageClassName="navbar__logo"
-            titleClassName={clsx("navbar__title", {
+            titleClassName={clsx('navbar__title', {
               [styles.hideLogoText]: isSearchBarExpanded,
             })}
           />
 
+          {leftItems.map((item, i) => {
+            return <NavbarItem {...item} key={i} />;
+          })}
+        </div>
+        <div className="navbar__items navbar__items--right">
           <SearchBar
             handleSearchBarToggle={setIsSearchBarExpanded}
             isSearchBarExpanded={isSearchBarExpanded}
           />
-          {leftItems.map((item, i) => {
-            console.log("item  ------  ", item);
-
-            return <NavbarItem {...item} key={i} />;
+          {rightItems.map((item, i) => {
+            switch (item.label) {
+              case 'GitHub':
+                return (
+                  <a
+                    target="_blank"
+                    href={item.to}
+                    className="mt-1 hidden md:inline-block navbar__item navbar__link"
+                  >
+                    <GitHub fill={isDarkTheme ? '#EAEAEA' : '#4C4C4C'} />
+                  </a>
+                );
+              case 'Docs':
+                return (
+                  <a
+                    href={item.to}
+                    className={`${
+                      isDarkTheme
+                        ? 'bg-gray-50 text-black'
+                        : 'bg-gray-900 text-white '
+                    } py-2 px-4 border-0 ml-4 rounded hidden md:inline-block navbar__item navbar__link`}
+                  >
+                    Get Started
+                    <RightArrow
+                      fill={isDarkTheme ? '#000' : '#fff'}
+                      className="ml-2"
+                    />
+                  </a>
+                );
+              case 'Theme':
+                return (
+                  !disableColorModeSwitch && (
+                    <button
+                      onClick={onToggleChange}
+                      className="bg-transparent border-0 hidden md:inline-block navbar__item navbar__link"
+                    >
+                      {isDarkTheme ? (
+                        <Sun fill={isDarkTheme ? '#EAEAEA' : '#4C4C4C'} />
+                      ) : (
+                        <Moon fill={isDarkTheme ? '#EAEAEA' : '#4C4C4C'} />
+                      )}
+                    </button>
+                    // <Toggle
+                    //   className={styles.displayOnlyInLargeViewport}
+                    //   aria-label="Dark mode toggle"
+                    //   checked={isDarkTheme}
+                    //   onChange={onToggleChange}
+                    // />
+                  )
+                );
+              case 'Discord':
+                return (
+                  <a
+                    target="_blank"
+                    href={item.to}
+                    className="mt-1 border-0 hidden md:inline-block navbar__item navbar__link"
+                  >
+                    <Discord fill={isDarkTheme ? '#EAEAEA' : '#4C4C4C'} />
+                  </a>
+                );
+              case 'Market':
+                return <NavbarItem {...item} className="ml-4" key={i} />;
+              default:
+                return <NavbarItem {...item} key={i} />;
+            }
           })}
-        </div>
-        <div className="navbar__items justify-center">
-          {centerItems.map((item, i) => (
-            <NavbarItem {...item} key={i} />
-          ))}
-        </div>
-        <div className="navbar__items navbar__items--right">
-          <a
-            // onClick={onToggleChange}
-            href="https://github.com/GeekyAnts/nativebase"
-            className="bg-transparent ml-3 mt-1 border-0"
-          >
-            <GitHub />
-          </a>
-          {rightItems.map((item, i) => (
-            <NavbarItem {...item} key={i} className="ml-3" />
-          ))}
-          {!disableColorModeSwitch && (
-            <button
-              onClick={onToggleChange}
-              className="bg-transparent ml-3 p-0 border-0"
-            >
-              {isDarkTheme ? <Sun /> : <Moon />}
-            </button>
-            // <Toggle
-            //   className={styles.displayOnlyInLargeViewport}
-            //   aria-label="Dark mode toggle"
-            //   checked={isDarkTheme}
-            //   onChange={onToggleChange}
-            // />
-          )}
-          <button
-            onClick={onToggleChange}
-            className="bg-gray-900 py-3 px-4 border-0 text-white ml-3 rounded"
-          >
-            Get Started <RightArrow className="ml-2" />
-          </button>
         </div>
       </div>
       <div
@@ -191,12 +202,12 @@ function Navbar() {
         <div className="navbar-sidebar__items">
           <div className="menu">
             <ul className="menu__list">
-              {centerItems.map((item, i) => (
-                <NavbarItem mobile {...item} onClick={hideSidebar} key={i} />
-              ))}
-              {items.map((item, i) => (
-                <NavbarItem mobile {...item} onClick={hideSidebar} key={i} />
-              ))}
+              {items
+                .slice(0)
+                .reverse()
+                .map((item, i) => (
+                  <NavbarItem mobile {...item} onClick={hideSidebar} key={i} />
+                ))}
             </ul>
           </div>
         </div>
