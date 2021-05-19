00cb3b40c4fadba668660c09dcc4b8ad4b572cc5
Nishan Bende <nishanbende@gmail.com>
2021-05-19 21:20:01 +0530

    test

--- clone-src.sh
+++ clone-src.sh
@@ -1,11 +1,2 @@
-SRC="NativeBase"
-BRANCH="v3-pre-beta"
-if [ ! -d "$SRC" ]; then
-  # Take action if $DIR exists. #
-  git clone https://github.com/GeekyAnts/NativeBase.git
-fi
 cd NativeBase
-git pull origin
-git checkout "$BRANCH"
-git pull origin "$BRANCH"
 yarn
