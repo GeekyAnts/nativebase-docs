d66ff83e53784ae184bc24e880940d6c3df3c0e4
Nishan Bende <nishanbende@gmail.com>
2021-06-07 21:54:20 +0530

    intro

--- src/components/NBHistory/index.js
+++ src/components/NBHistory/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import useThemeContext from '@theme/hooks/useThemeContext';
+
 export default function NBHistory() {
+  const { isDarkTheme } = useThemeContext();
+  const textClassName = isDarkTheme ? '' : 'text-white';
+  const lineColor = isDarkTheme ? 'border-gray-100' : 'border-gray-200';
+  const bgColor = isDarkTheme ? 'bg-gray-600' : 'bg-gray-500';
+
   return (
-    <div className="border-0 border-l-4 border-gray-100 border-solid">
+    <div
+      className={
+        'border-0 border-l-4 border-solid ' + textClassName + ' ' + lineColor
+      }
+    >
       <div className="flex items-center">
         <div
           className="w-5 h-5 bg-indigo-500 rounded-full"
           style={{ marginLeft: '-12px' }}
         ></div>
         <div
-          className="ml-8 py-4 px-8 bg-indigo-600 shadow-xl rounded-md"
+          className={'ml-8 py-6 px-8 bg-indigo-500 shadow-xl rounded-md'}
           style={{ maxWidth: '80%', width: '500px' }}
         >
           <h4>v3</h4>
@@ -26,7 +37,7 @@ export default function NBHistory() {
           style={{ marginLeft: '-12px' }}
         ></div>
         <div
-          className="ml-8 py-4 px-8 bg-gray-700 shadow-xl rounded-md"
+          className={'ml-8 py-6 px-8 shadow-xl rounded-md ' + bgColor}
           style={{ maxWidth: '80%', width: '500px' }}
         >
           <h4>v2</h4>
@@ -44,7 +55,7 @@ export default function NBHistory() {
           style={{ marginLeft: '-12px' }}
         ></div>
         <div
-          className="ml-8 py-4 px-8 bg-gray-700 shadow-xl rounded-md"
+          className={'ml-8 py-6 px-8 shadow-xl rounded-md ' + bgColor}
           style={{ maxWidth: '80%', width: '500px' }}
         >
           <h4>v1</h4>
