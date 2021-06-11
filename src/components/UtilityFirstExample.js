a5c2527d2393e0028932379af79db64c97697b37
Nishan Bende <nishanbende@gmail.com>
2021-06-11 22:33:56 +0530

    e

--- src/components/UtilityFirstExample.js
+++ src/components/UtilityFirstExample.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import useThemeContext from '@theme/hooks/useThemeContext';
+import { NativeBaseProvider, Box } from 'native-base';
 
 export function UtilityFirstExample() {
   const { isDarkTheme } = useThemeContext();
   const bgColor = isDarkTheme ? 'bg-gray-800' : 'bg-indigo-50';
 
   return (
-    <div className="tailwind">
-      <div className={'p-8 text-center rounded-xl mb-8 ' + bgColor}>
-        <img src="/img/ang-card.png" style={{ height: '200px' }} />
+    <NativeBaseProvider>
+      <div className="tailwind">
+        <div className={'p-8 text-center rounded-xl mb-8 ' + bgColor}>
+          <img src="/img/ang-card.png" style={{ height: '200px' }} />
+        </div>
       </div>
-    </div>
+      <Box bg="cyan.500" p={3}>
+        23
+      </Box>
+    </NativeBaseProvider>
   );
 }
