c32d978959ebd4997bbea0653c129328a4be70c8
cyrus25 <cyruspassi@gmail.com>
2021-07-08 19:03:40 +0530

    send document url

--- static/js/google-analytics.js
+++ static/js/google-analytics.js
@@ -1,12 +1,21 @@
 let timesRun = 0;
 let intervalInstance = setInterval(() => {
   timesRun = timesRun + 1;
-  if (timesRun > 3) clearInterval(intervalInstance);
   if (document && document.title !== '' && document.title !== 'NativeBase') {
     function gtag() {
       dataLayer.push(arguments);
     }
     window.dataLayer = window.dataLayer || [];
+
+    if (timesRun > 3) {
+      let pageTitle =
+        location.pathname === '/' ? 'NativeBase | NativeBase' : location.href;
+      gtag('config', 'G-ZTSFCSJK8X', {
+        page_title: pageTitle,
+      });
+      clearInterval(intervalInstance);
+    }
+
     gtag('js', new Date());
     gtag('config', 'G-ZTSFCSJK8X');
     clearInterval(intervalInstance);
