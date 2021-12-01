16af1a5c07161fdbda0fb793def6f8688862a9c7
meenu <makkarmini007@gmail.com>
2021-12-01 17:34:48 +0530

    responsiveness and slider integration

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -25,6 +25,8 @@ import router, { Router, useRouter } from "next/router";
 import path from "path";
 import Sidebar from "../src/new-components/Sidebar";
 import Navbar from "../src/new-components/Navbar";
+import MobileNavbar from "../src/new-components/MobileNavbar";
+
 import { AppContext } from "../src/AppContext";
 import MainContent from "../src/new-components/MainContent";
 import MobileSidebar from "../src/new-components/MobileSidebar";
@@ -39,14 +41,8 @@ function Layout({
   // console.log("Sidebar", sidebar);
   const { activeVersion, setActiveVersion } = useContext(AppContext);
   const router = useRouter();
-  const [isSlideOpen, setSlideOpen] = useState(false);
-  const [isCollapsible, setIsCollapsible] = useState(false);
 
   useEffect(() => {
-    window.document.body.addEventListener("click", () => {
-      setSlideOpen(false);
-    });
-
     const currentPathArray = window?.location.href.split("/");
     // console.log(currentPathArray);
 
@@ -72,18 +68,12 @@ function Layout({
         <title>Layouts Example</title>
       </Head>
       <Box h="100vh">
-        <Navbar />
-
-        <IconButton
-          icon={<HamburgerIcon size="7" />}
-          // @ts-ignore
-          onPress={() => setSlideOpen(!isSlideOpen)}
-          accessibilityLabel="Mobile Menu"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-          mr="2"
-          // display={{ base: "flex", lg: "none" }}
-        />
+        <Box display={{ base: "none", lg: "flex" }}>
+          <Navbar />
+        </Box>
+        <Box display={{ base: "flex", lg: "none" }}>
+          <MobileNavbar />
+        </Box>
 
         <HStack flex="1">
           {/* leftsidebar only show on big devices */}
@@ -97,81 +87,6 @@ function Layout({
             <MobileSidebar sidebar={sidebar} />
           </Box>
         </HStack>
-        <Slide
-          in={isSlideOpen}
-          placement="left"
-          flex="1"
-          height="100%"
-          zIndex="2"
-        >
-          <Box
-            zIndex="1"
-            w="300px"
-            height="100vh"
-            bg={"black"}
-            overflow="scroll"
-            _web={{
-              // @ts-ignore
-              style: {
-                "transition-property": "all",
-                transitionDuration: ".3s",
-                "transition-timing-function": "cubic-bezier(.4,0,.2,1)",
-              },
-            }}
-          >
-            <VStack px={{ base: "2", sm: "3" }} pt="2" pb="3" space="2">
-              <HStack
-                space="10"
-                px="3"
-                py="2"
-                borderBottomWidth="1"
-                borderBottomColor="gray.200"
-                alignItems="center"
-              >
-                gekko
-              </HStack>
-              <Box pt="3">
-                <Pressable
-                  // className="flex justify-between px-3 py-1  rounded-md cursor-pointer text-gray-500 dark:text-gray-400"
-                  rounded="md"
-                  // @ts-ignore
-                  onPress={() => {
-                    setIsCollapsible(!isCollapsible);
-                  }}
-                >
-                  <HStack justifyContent="space-between" px="3" py="1">
-                    <Text
-                      // @ts-ignore
-                      accessibilityLabel="Versions"
-                      fontSize="md"
-                      fontWeight="medium"
-                      color={"gray.400"}
-                    >
-                      Versions
-                    </Text>
-
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      transform={isCollapsible ? "rotate(90)" : ""}
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="gray"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M9 5l7 7-7 7"
-                      />
-                    </svg>
-                  </HStack>
-                </Pressable>
-              </Box>
-            </VStack>
-          </Box>
-        </Slide>
       </Box>
     </>
   );
