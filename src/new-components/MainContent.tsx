16f3d18b3b2332d5b0ad038748a1af3c08ee53ab
meenu <makkarmini007@gmail.com>
2021-12-23 12:04:21 +0530

    toc scroll spy in progress

--- src/new-components/MainContent.tsx
+++ src/new-components/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   Box,
   Heading,
@@ -36,10 +36,40 @@ import {
 import { AppContext } from "../AppContext";
 import * as docComponents from "../components";
 import { isLatestVersionSlug } from "../utils";
+import { ScrollContext } from "./ScrollContext";
 
 export default function MainContent(props: any) {
+  const { tocArray } = props;
+  const [timestamp, setTimestamp] = useState("");
+  return (
+    <>
+      <ScrollContext.Provider value={{ timestamp, setTimestamp }}>
+        <SubMainContent props={props} />
+        {props.showToc && (
+          <Box display={{ base: "none", lg: "flex" }}>
+            <Toc tocArray={tocArray} />
+          </Box>
+        )}
+      </ScrollContext.Provider>
+    </>
+  );
+}
+const SubMainContent = ({ props }: any) => {
+  const { timestamp, setTimestamp } = useContext(ScrollContext);
+  function handleScroll(e: any) {
+    setTimestamp(e.timeStamp);
+  }
+  return (
+    <ScrollView scrollEventThrottle={16} onScroll={handleScroll}>
+      <SubMainContent2 props={props} />
+    </ScrollView>
+  );
+};
+
+// eslint-disable-next-line react/display-name
+const SubMainContent2 = React.memo(({ props }: any) => {
   const { Tabs, ...RemNBComponents } = NBComponents;
-  const { content, tocArray, pages, frontMatter } = props;
+  const { content, pages, frontMatter } = props;
   const { activeVersion, setActiveSidebarItem } = useContext(AppContext);
   const components = {
     h1: Heading1,
@@ -54,7 +84,8 @@ export default function MainContent(props: any) {
     a: Anchor,
     code: ({ children, ...props }: any) => {
       if (props?.isLive) return <Playground {...props}>{children}</Playground>;
-      else if (props?.isShowcase) return <Showcase {...props}>{children}</Showcase>;
+      else if (props?.isShowcase)
+        return <Showcase {...props}>{children}</Showcase>;
       else return <CodeBlock>{children}</CodeBlock>;
     },
     table: TableBox,
@@ -66,86 +97,73 @@ export default function MainContent(props: any) {
     ...RemNBComponents,
   };
   return (
-    <>
-      <ScrollView>
-        <Box px={{ base: "6", xl: "16" }} py="10">
-          <Heading>
-            {frontMatter && frontMatter.title
-              ? frontMatter.title
-              : pages.currentPage.title}
-          </Heading>
-          <MDXRemote {...content} components={components} />
-          <HStack justifyContent="space-between" my="12">
-            {pages.previousPage && (
-              <Pressable
-                mr="auto"
-                onPress={() => setActiveSidebarItem(pages.previousPage.id)}
-              >
-                <Link
-                  passHref
-                  href={`${
-                    isLatestVersionSlug(activeVersion)
-                      ? ""
-                      : activeVersion + "/"
-                  }${pages.previousPage.id}`}
+    <Box px={{ base: "6", xl: "16" }} py="10">
+      <Heading>
+        {frontMatter && frontMatter.title
+          ? frontMatter.title
+          : pages.currentPage.title}
+      </Heading>
+      <MDXRemote {...content} components={components} />
+      <HStack justifyContent="space-between" my="12">
+        {pages.previousPage && (
+          <Pressable
+            mr="auto"
+            onPress={() => setActiveSidebarItem(pages.previousPage.id)}
+          >
+            <Link
+              passHref
+              href={`${
+                isLatestVersionSlug(activeVersion) ? "" : activeVersion + "/"
+              }${pages.previousPage.id}`}
+            >
+              <VStack>
+                <Text
+                  _light={{ color: "pageNavigationHeadingLight" }}
+                  _dark={{ color: "pageNavigationHeadingDark" }}
                 >
-                  <VStack>
-                    <Text
-                      _light={{ color: "pageNavigationHeadingLight" }}
-                      _dark={{ color: "pageNavigationHeadingDark" }}
-                    >
-                      Previous
-                    </Text>
-                    <Text
-                      fontSize="lg"
-                      _light={{ color: "pageNavigationMainTitleLight" }}
-                      _dark={{ color: "pageNavigationMainTitleDark" }}
-                    >
-                      {pages.previousPage.title}
-                    </Text>
-                  </VStack>
-                </Link>
-              </Pressable>
-            )}
-            {pages.nextPage && (
-              <Pressable
-                ml="auto"
-                onPress={() => setActiveSidebarItem(pages.nextPage.id)}
-              >
-                <Link
-                  passHref
-                  href={`${
-                    isLatestVersionSlug(activeVersion)
-                      ? ""
-                      : activeVersion + "/"
-                  }${pages.nextPage.id}`}
+                  Previous
+                </Text>
+                <Text
+                  fontSize="lg"
+                  _light={{ color: "pageNavigationMainTitleLight" }}
+                  _dark={{ color: "pageNavigationMainTitleDark" }}
                 >
-                  <VStack>
-                    <Text
-                      _light={{ color: "pageNavigationHeadingLight" }}
-                      _dark={{ color: "pageNavigationHeadingDark" }}
-                    >
-                      Next
-                    </Text>
-                    <Text
-                      fontSize="lg"
-                      _light={{ color: "pageNavigationMainTitleLight" }}
-                      _dark={{ color: "pageNavigationMainTitleDark" }}
-                    >
-                      {pages.nextPage.title}
-                    </Text>
-                  </VStack>
-                </Link>
-              </Pressable>
-            )}
-          </HStack>
-        </Box>
-      </ScrollView>
-      {props.showToc && (
-        <Box display={{ base: "none", lg: "flex" }}>
-          <Toc tocArray={tocArray} />
-        </Box>
-      )}
-    </>
+                  {pages.previousPage.title}
+                </Text>
+              </VStack>
+            </Link>
+          </Pressable>
+        )}
+        {pages.nextPage && (
+          <Pressable
+            ml="auto"
+            onPress={() => setActiveSidebarItem(pages.nextPage.id)}
+          >
+            <Link
+              passHref
+              href={`${
+                isLatestVersionSlug(activeVersion) ? "" : activeVersion + "/"
+              }${pages.nextPage.id}`}
+            >
+              <VStack>
+                <Text
+                  _light={{ color: "pageNavigationHeadingLight" }}
+                  _dark={{ color: "pageNavigationHeadingDark" }}
+                >
+                  Next
+                </Text>
+                <Text
+                  fontSize="lg"
+                  _light={{ color: "pageNavigationMainTitleLight" }}
+                  _dark={{ color: "pageNavigationMainTitleDark" }}
+                >
+                  {pages.nextPage.title}
+                </Text>
+              </VStack>
+            </Link>
+          </Pressable>
+        )}
+      </HStack>
+    </Box>
   );
-}
+});
