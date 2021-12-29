084170c77c9f2770a68811297f0f1ecd454c3672
Rohit Singh <rohit99.ind@gmail.com>
2021-12-29 13:09:04 +0530

    feat: admonitions

--- src/new-components/markdown-components/Ul.tsx
+++ src/new-components/markdown-components/Ul.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { VStack } from "native-base";
 export const Ul = ({ children }: any) => {
   return (
-    <VStack space="4" my="2">
+    <VStack space="3" my="2">
       {children}
     </VStack>
   );
