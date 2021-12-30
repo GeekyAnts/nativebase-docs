10795a01b18cbacfa7e0cab48fcc2f771e7b5adb
Vidhi Kataria <vidhikataria@Vidhi-MacBook-Pro.local>
2021-12-30 12:55:46 +0530

    few error fixes

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -6,7 +6,7 @@ import {
   ScrollView,
   useColorModeValue,
   useToken,
-} from "native-base";
+} from "../nb";
 import path from "path";
 import Sidebar from "../src/new-components/Sidebar";
 import Navbar from "../src/new-components/Navbar";
