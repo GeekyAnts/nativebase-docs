635ce1eb5ec258a8c492248d3ab41c234436c7a7
meenu <makkarmini007@gmail.com>
2022-01-11 13:37:10 +0530

    added changelog and addedskeleton and aspect ratio components

--- sidebars.js
+++ sidebars.js
@@ -52,7 +52,7 @@ module.exports = {
       label: 'Layout',
       collapsed: false,
       items: [
-        // 'aspectRatio',
+        'aspectRatio',
         'box',
         'center',
         'container',
