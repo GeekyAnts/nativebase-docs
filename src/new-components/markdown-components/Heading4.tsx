64afcfdc61b65b00907304d7a32aadf1b651b299
Rohit Singh <rohit99.ind@gmail.com>
2022-02-14 11:41:29 +0530

    fix: heading ids

--- src/new-components/markdown-components/Heading4.tsx
+++ src/new-components/markdown-components/Heading4.tsx
@@ -6,13 +6,12 @@ export const Heading4 = ({ children }: any) => {
   const [content, id] = getHeadingMeta(children);
   return (
     <>
-      <Heading display="none" nativeID="DocSearch-h4" >{content}</Heading>
       <Heading
         my="2"
         fontSize="md"
         color={useColorModeValue("gray.700", "gray.300")}
         // @ts-ignore
-        nativeID={id}
+        nativeID={"h4" + id}
       >
         {content}
       </Heading>
