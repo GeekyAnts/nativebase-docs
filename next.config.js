5725c5554498ac9ddfdfed8fdc1d1ebdafc783f7
Vidhi Kataria <GeekyAnts@Vidhi-Kataria-B453-MacBook-Air.local>
2022-02-28 13:43:20 +0530

    flicker issue fixed

--- next.config.js
+++ next.config.js
@@ -1,118 +1,99 @@
-const withFonts = require("next-fonts");
+const { withNativebase } = require("@native-base/next-adapter");
+const path = require("path");
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
-const path = require("path");
+
 // const { withExpo } = require("@expo/next-adapter");
 const redirectsJSON = require("./redirects.json");
-const withPlugins = require("next-compose-plugins");
-const withTM = require("next-transpile-modules")([
-  "native-base",
-  "react-native",
-  "react-native-svg",
-  "react-native-web",
-  "react-native-safe-area-context",
-  "@react-aria/visually-hidden",
-  "@react-native-aria/button",
-  "@react-native-aria/checkbox",
-  "@react-native-aria/combobox",
-  "@react-native-aria/focus",
-  "@react-native-aria/interactions",
-  "@react-native-aria/listbox",
-  "@react-native-aria/overlays",
-  "@react-native-aria/radio",
-  "@react-native-aria/slider",
-  "@react-native-aria/tabs",
-  "@react-native-aria/utils",
-  "@react-stately/combobox",
-  "@react-stately/radio",
-  "react-native-vector-icons",
-  "@native-base/next-adapter",
-  "@native-base/icons",
-  // "@expo/vector-icons",
-  // "react-native-safe-area-context",
-  // "@react-aria/visually-hidden",
-  // "@react-native-aria/button",
-  // "@react-native-aria/checkbox",
-  // "@react-native-aria/combobox",
-  // "@react-native-aria/focus",
-  // "@react-native-aria/interactions",
-  // "@react-native-aria/listbox",
-  // "@react-native-aria/overlays",
-  // "@react-native-aria/radio",
-  // "@react-native-aria/slider",
-  // "@react-native-aria/tabs",
-  // "@react-native-aria/utils",
-  // "@react-stately/combobox",
-  // "@react-stately/radio",
-]);
-
-module.exports = withPlugins([[withTM], [withFonts], [withMDX]], {
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  // webpack: (config) => {
-  //   config.resolve.alias = {
-  //     ...(config.resolve.alias || {}),
-  //     // Transform all direct `react-native` imports to `react-native-web`
-  //     "react-native$": "react-native-web",
-  //   };
-  //   config.resolve.extensions = [
-  //     ".web.js",
-  //     ".web.ts",
-  //     ".web.tsx",
-  //     ...config.resolve.extensions,
-  //   ];
-  //   return config;
-  // },
-  async redirects() {
-    return [
-      {
-        source: "/discord",
-        destination: "https://discord.com/invite/TSgCw2UPmb",
-        permanent: true,
-      },
-      {
-        source: "/blogs",
-        destination: "https://nativebase.io/blogs",
-        permanent: true,
-      },
-      ...redirectsJSON,
-    ];
-  },
-  images: {
-    domains: [
-      "docs.nativebase.io",
-      "media-exp1.licdn.com",
-      "mir-s3-cdn-cf.behance.net",
-      "avatars.githubusercontent.com",
-      "images.opencollective.com",
-      "pbs.twimg.com",
-    ],
-  },
-  env: {
-    environment: process.env.ENVIRONMENT,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ttf$/,
-      loader: "url-loader", // or directly file-loader
-      include: path.resolve(__dirname, "node_modules/@native-base/icons"),
-    });
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      "react-native$": "react-native-web",
-      "@native-base/icons": "react-native-vector-icons",
-    };
-    config.resolve.extensions = [
-      ".web.js",
-      ".web.ts",
-      ".web.tsx",
-      ...config.resolve.extensions,
-    ];
-    return config;
-  },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+module.exports = withNativebase({
+  dependencies: ["@native-base/icons"],
+  plugins: [[withMDX]],
+  nextConfig: {
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    // webpack: (config) => {
+    //   config.resolve.alias = {
+    //     ...(config.resolve.alias || {}),
+    //     // Transform all direct `react-native` imports to `react-native-web`
+    //     "react-native$": "react-native-web",
+    //   };
+    //   config.resolve.extensions = [
+    //     ".web.js",
+    //     ".web.ts",
+    //     ".web.tsx",
+    //     ...config.resolve.extensions,
+    //   ];
+    //   return config;
+    // },
+    async redirects() {
+      return [
+        {
+          source: "/discord",
+          destination: "https://discord.com/invite/TSgCw2UPmb",
+          permanent: true,
+        },
+        {
+          source: "/blogs",
+          destination: "https://nativebase.io/blogs",
+          permanent: true,
+        },
+        ...redirectsJSON,
+      ];
+    },
+    images: {
+      domains: [
+        "docs.nativebase.io",
+        "media-exp1.licdn.com",
+        "mir-s3-cdn-cf.behance.net",
+        "avatars.githubusercontent.com",
+        "images.opencollective.com",
+        "pbs.twimg.com",
+      ],
+    },
+    env: {
+      environment: process.env.ENVIRONMENT,
+    },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.ttf$/,
+        loader: "url-loader", // or directly file-loader
+        include: path.resolve(__dirname, "node_modules/@native-base/icons"),
+      });
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        // Transform all direct `react-native` imports to `react-native-web`
+        "react-native$": "react-native-web",
+        // "@native-base/icons": "react-native-vector-icons",
+      };
+      config.resolve.extensions = [
+        ".web.js",
+        ".web.ts",
+        ".web.tsx",
+        ...config.resolve.extensions,
+      ];
+      return config;
+    },
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+    // webpack: (config, options) => {
+    //   config.module.rules.push({
+    //     test: /\.ttf$/,
+    //     loader: "url-loader", // or directly file-loader
+    //     include: path.resolve(__dirname, "node_modules/@native-base/icons"),
+    //   });
+    //   config.resolve.alias = {
+    //     ...(config.resolve.alias || {}),
+    //     "react-native$": "react-native-web",
+    //   };
+    //   config.resolve.extensions = [
+    //     ".web.js",
+    //     ".web.ts",
+    //     ".web.tsx",
+    //     ...config.resolve.extensions,
+    //   ];
+    //   return config;
+    // },
   },
 });
