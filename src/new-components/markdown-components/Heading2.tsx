ad268bb07506947339c9afc688f904053c904014
amars29 <amars@geekyants>
2022-05-04 16:30:32 +0530

    upgraded native-base

--- src/new-components/markdown-components/Heading2.tsx
+++ src/new-components/markdown-components/Heading2.tsx
@@ -1,27 +1,32 @@
-import React from "react";
-import { Heading, HStack, Icon, Pressable, Link } from "native-base";
-import { getHeadingMeta } from "../../utils";
-import { Fontisto } from "@expo/vector-icons";
+import React from 'react';
+import { Heading, HStack, Icon, Pressable, Link } from 'native-base';
+import { getHeadingMeta } from '../../utils';
+import { Fontisto } from '@expo/vector-icons';
 
 export const Heading2 = ({ children }: any) => {
   const [content, id] = getHeadingMeta(children);
 
   return (
-    <Pressable mt="7" mb="4" _web={{ cursor: "default" }}>
+    <Pressable
+      mt="7"
+      mb="4"
+      // @ts-ignore
+      _web={{ cursor: 'default' }}
+    >
       {({ isHovered }) => {
         return (
           <HStack alignItems="center" space="2">
             <Heading
               fontSize="2xl"
-              _dark={{ color: "gray.300" }}
-              _light={{ color: "gray.700" }}
+              _dark={{ color: 'gray.300' }}
+              _light={{ color: 'gray.700' }}
               // @ts-ignore
-              nativeID={"h2-" + id}
+              nativeID={'h2-' + id}
             >
               {content}
             </Heading>
             {isHovered && (
-              <Link href={"#" + "h2-" + id}>
+              <Link href={'#' + 'h2-' + id}>
                 <Icon as={Fontisto} name="link" color="coolGray.500" size="4" />
               </Link>
             )}
