21aed4e56d0e613b82f8cc370643176c0db984e4
ankit-tailor <tailorankit3345@gmail.com>
2022-11-15 16:19:41 +0530

    fix: code block design for bash

--- src/new-components/markdown-components/CodeBlock/CodeBlock.tsx
+++ src/new-components/markdown-components/CodeBlock/CodeBlock.tsx
@@ -14,11 +14,27 @@ import {
   Icon,
   Button,
   useColorModeValue,
+  HStack,
 } from "native-base";
 
-export const CodeBlock = ({ children, props }: any) => {
+const useIsOverflow = (ref: any) => {
+  const [isOverflow, setIsOverflow] = React.useState(false);
+
+  React.useLayoutEffect(() => {
+    const { current } = ref;
+
+    if (current) setIsOverflow(current.scrollWidth > current.clientWidth);
+  }, [ref]);
+
+  return isOverflow;
+};
+
+export const CodeBlock = ({ children, isBash, ...props }: any) => {
   // const [parsedCode, setParsedCode] = React.useState(children);
   const [copied, setCopied] = React.useState(false);
+  const [overflow, setOverflow] = React.useState<any>("hidden");
+  const ref = React.useRef(null);
+  const isContentOverFlow = useIsOverflow(ref);
 
   const { onCopy } = useClipboard();
 
@@ -31,75 +47,113 @@ export const CodeBlock = ({ children, props }: any) => {
     }, 2000);
   }
 
-  const bgColor = useColorModeValue("#f9fafb", "#171E2E");
-  const borderColor = useColorModeValue("#e5e7eb", "#1e293b");
-  return (
-    <Highlight
-      {...defaultProps}
-      code={children.trim("\n")}
-      language="tsx"
-      theme={useColorModeValue(github, paleNight)}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <Box w="100%" position="relative">
-          <Button
-            size="sm"
-            opacity={60}
-            position={"absolute"}
-            variant="unstyled"
-            _light={{ bg: "coolGray.300" }}
-            _dark={{ bg: "coolGray.800" }}
-            bg="coolGray.800"
-            _hover={{ bg: "coolGray.600" }}
-            top={3}
-            right={4}
-            px="2"
-            p="1"
-            _text={{
-              fontSize: "sm",
-              lineHeight: "1.5em",
-              // hack for now
-              fontFamily:
-                "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-            }}
-            onPress={handleCopy}
-            rightIcon={
-              <Icon
-                as={expoVectorIcons?.Ionicons}
-                name={copied ? "copy" : "copy-outline"}
-                size="4"
-              />
-            }
-          >
-            {!copied ? "Copy" : "Copied"}
-          </Button>
+  let bgDarkColor = '#171E2E'
+  let bgLightColor = '#f9fafb'
 
-          <pre
-            className={className}
-            style={{
-              ...style,
-              borderRadius: "8px",
-              padding: "16px",
-              paddingBottom: "8px",
-              margin: "0px",
-              overflow: "scroll",
-              backgroundColor: bgColor,
-              borderWidth: "1",
-              borderColor: borderColor,
-              fontSize: "14px",
-              lineHeight: "normal",
-            }}
-          >
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })} key={i}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} key={key} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        </Box>
-      )}
-    </Highlight>
+  if(isBash) {
+    bgDarkColor = 'blueGray.800';
+    bgLightColor = 'coolGray.200'
+  }
+
+  const bgColor = useColorModeValue(bgLightColor, bgDarkColor);
+  const borderColor = useColorModeValue("#e5e7eb", "#1e293b");
+
+  const handleMouseOver = () => {
+    setOverflow("auto");
+  };
+
+  const handleMouseOut = () => {
+    setOverflow("hidden");
+  };
+
+  return (
+    <HStack bg={bgColor} rounded="lg" {...props}>
+      <Highlight
+        {...defaultProps}
+        code={children.trim("\n")}
+        language="tsx"
+        theme={useColorModeValue(github, paleNight)}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <Box flexDirection={"row"} w="100%" justifyContent={"space-between"}>
+            <pre
+              ref={ref}
+              className={className}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+              style={{
+                ...style,
+                borderRadius: "8px",
+                padding: "16px",
+                paddingBottom:
+                  overflow === "auto" && isContentOverFlow ? "8px" : "16px",
+                margin: "0px",
+                backgroundColor: bgColor,
+                borderWidth: "1",
+                borderColor: borderColor,
+                fontSize: "14px",
+                lineHeight: "normal",
+                overflowX: overflow,
+              }}
+            >
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })} key={i}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} key={key} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+            <Box justifyContent="flex-start" mt="2.5" mx="3">
+              <Button
+                variant="unstyled"
+                _light={{
+                  bg: "coolGray.50",
+                  _hover: {
+                    bg: "coolGray.400",
+                    _text: { color: "coolGray.50" },
+                    _icon: { color: "coolGray.50" },
+                  },
+                  _pressed: {
+                    bg: "coolGray.400",
+                    _text: { color: "coolGray.50" },
+                    _icon: { color: "coolGray.50" },
+                  },
+                  _text: { color: "muted.500" },
+                  _icon: { color: "muted.500" },
+                }}
+                _dark={{
+                  bg: "blueGray.700",
+                  _hover: { bg: "blueGray.600" },
+                  _pressed: { bg: "blueGray.500" },
+                  _text: { color: "muted.400" },
+                  _icon: { color: "muted.400" },
+                }}
+                px="2"
+                py="1"
+                p="1"
+                _text={{
+                  fontSize: "sm",
+                  lineHeight: "1.5em",
+                  // hack for now
+                  fontFamily:
+                    "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+                }}
+                onPress={handleCopy}
+                rightIcon={
+                  <Icon
+                    as={expoVectorIcons?.Ionicons}
+                    name={copied ? "copy" : "copy-outline"}
+                    size="4"
+                  />
+                }
+              >
+                {!copied ? "Copy" : "Copied"}
+              </Button>
+            </Box>
+          </Box>
+        )}
+      </Highlight>
+    </HStack>
   );
 };
